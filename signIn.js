4dbc5359edef6f9ee3ba8625d6dbda8ebbc04645
Neha Hafeez <85940284+Neha-Hafeez@users.noreply.github.com>
2023-06-13 01:15:08 +0500

    Add files via upload

---
+++ signIn.js
@@ -0,0 +1,38 @@
+console.log("connected")
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCePdbMyYhGEtt-a13IIoEBF-PBBploEwo",
+    authDomain: "introvert-heaven.firebaseapp.com",
+    projectId: "introvert-heaven",
+    storageBucket: "introvert-heaven.appspot.com",
+    messagingSenderId: "346772784744",
+    appId: "1:346772784744:web:7e445e277d999494b21ddd"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+// Text Fields
+let email = document.getElementById("inputEmail1");
+let password = document.getElementById("inputPassword1");
+
+// Sign In
+let signInBtn = document.getElementById("btn2");
+
+signInBtn.addEventListener('click', e=> {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email.value, password.value).then(()=> {
+        console.log("login invoked");
+        alert("Login Successful")
+        location = "userProfile.html";
+    
+       }).catch(err => {
+    
+        console.log(err.message);
+       })
+});
